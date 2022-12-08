d09b264c652c8815cb3ce41076f2a2a2ac6b120b
Paul Richard <paulrpm@gmail.com>
2022-12-08 04:07:54 -0300

    checking if user is logged in and redirecting when acessing home

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
@@ -13,6 +14,15 @@ class Controller extends BaseController
 
     public function index()
     {
+        $user = Auth::user();
+
+        if ($user && $user->isAdmin) {
+            return redirect()->intended('admin');
+        }
+        if ($user && $user->isAgent) {
+            return redirect()->intended('agent');
+        }
+
         return view('index');
     }
 }
